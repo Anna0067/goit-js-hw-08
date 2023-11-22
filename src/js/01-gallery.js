2eb1b4add04532f640caecaa2ba5b9b6474588c7
Anna0067 <129055045+Anna0067@users.noreply.github.com>
2023-11-22 17:14:41 +0100

    correction

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,33 +3,9 @@ import { galleryItems } from './gallery-items';
 import simpleLightbox from 'simplelightbox';
 import 'simpleLightbox/dist/simple-lightbox.min.css';
 
-const gallery = new SimpleLightbox('#image-gallery a');
-
 // Change code below this line
 
 const galleryContainer = document.querySelector('ul.gallery');
-let currentLightbox = null;
-
-galleryContainer.addEventListener('click', event => {
-  event.preventDefault();
-  if (event.target.tagName === 'IMG') {
-    const originalSrc = event.target.dataset.source;
-    const lightbox = basicLightbox.create(
-      `<img src="${originalSrc}" alt="${event.target.alt}">`
-    );
-    lightbox.show();
-    currentLightbox = lightbox;
-
-    document.addEventListener('keydown', handleKeyPress);
-  }
-});
-const handleKeyPress = event => {
-  if (event.key === 'Escape' && currentLightbox) {
-    currentLightbox.close();
-    currentLightbox = null;
-    document.removeEventListener('keydown', handleKeyPress);
-  }
-};
 
 const images = galleryItems.map(item => {
   return `<div class = "gallery__item">
@@ -43,6 +19,8 @@ const images = galleryItems.map(item => {
     </div>`;
 });
 galleryContainer.insertAdjacentHTML('beforeend', images.join(''));
+
+const gallery = new SimpleLightbox('#image-gallery a');
 
 console.log(galleryItems);
 
